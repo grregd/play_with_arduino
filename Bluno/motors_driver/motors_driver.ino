3958aa05b284c37d2fe4f19405fc45550ba81ca8
Grzegorz Dulewicz <grzegorz.dulewicz@tieto.com>
2018-03-14 16:19:42 +0100

    Initial version

---
+++ Bluno/motors_driver/motors_driver.ino
@@ -0,0 +1,20 @@
+#include <Wire.h>
+
+const auto DRIVER_ADDRESS_TWI = 0;
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+
+ if (Serial.available())  {
+//    Serial.write(Serial.read());//send what has been received
+//    Serial.println();   //print line feed character
+  }
+    
+  Wire.beginTransmission(DRIVER_ADDRESS_TWI);
+  Wire.write(1);
+  Wire.endTransmission();
+  delay(1000);
+}
